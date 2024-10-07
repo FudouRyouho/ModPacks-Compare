62c225ba24f08a279c40e362864d50307de66d31
FudouRyouho <fudozaraki001@hotmail.com>
2024-10-07 14:21:00 -0300

    Declared error Solved

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import ModpackComparator from './components/ModpackComparator.tsx'
 import Upload from './components/upload.tsx'
 
 import './styles/output.css'
